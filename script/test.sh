a988df4cea6222bc56fb637c8df20bfcee97bd1c
ahsansabir30 <ahsansabir30@gmail.com>
2022-09-29 11:35:18 +0100

    fixed test script

--- script/test.sh
+++ script/test.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
-cd front-end
-# install nodejs
-curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+# Install chrome driver and make it env variable
+sudo apt install unzip
+sudo apt-get install -y chromium-browser
+export CHROME_BIN=/usr/bin/chromium-browser
+
+# install nodejs version 12
+curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-# install npm 
-sudo apt-get install npm
-sudo npm install -g @angular/cli
-sudo npm install --save-dev @angular/cli@latest
-rm ./frontend/spring-petclinic-angular/package-lock.json
-sudo npm install
-ng test
+# install angular cli version 11.2.x
+sudo npm install -g @angular/cli@11.2.x
+cd front-end && cd spring-petclinic-angular
+npm install --save-dev @angular/cli@11.2.x
+rm -f package-lock.json
+npm install
+
+# run test using the chromedriver installed above
+ng test --browsers=ChromeHeadless --watch=false
