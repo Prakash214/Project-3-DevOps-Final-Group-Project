2e773be16a59824ea02448a33fa3402cc37b3054
ahsansabir30 <ahsansabir30@gmail.com>
2022-10-06 10:46:26 +0100

    updating terraform and docker

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -1,10 +1,20 @@
-resource "aws_security_group" "node_group_one" {
-  name_prefix = "node_group_one"
+resource "aws_security_group" "node_group_sec" {
+  name_prefix = "node_group_sec"
   vpc_id      = module.vpc.vpc_id
-
+  
   ingress {
     from_port = 22
     to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  ingress {
+    from_port = 80
+    to_port   = 80
     protocol  = "tcp"
 
     cidr_blocks = [
